fc93eb1352d48c2c6eca48701b4701882021cc5c
Jindřiška Paďourková <padourkova.jindriska@gmail.com>
2021-04-14 20:30:54 +0200

    křížky a kroužky

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const circlePlays = document.querySelector('#cross_symbole');
-const crossPlays = document.querySelector('#circle_symbole');
+const circlePlays = document.querySelector('#circle_symbole');
+const crossPlays = document.querySelector('#cross_symbole');
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    crossPlays.classList.toggle('circle_not_plays');
-    circlePlays.classList.toggle('cross_plays');
+    circlePlays.classList.toggle('circle_not_plays');
+    crossPlays.classList.toggle('cross_plays');
 
-    if (circlePlays.className === 'cross_plays') {
-      button.classList.toggle('fill_cross');
+    if (circlePlays.className === 'circle_plays') {
+      button.classList.add('fill_cross');
       button.disabled = 'true';
     } else {
-      button.classList.toggle('fill_circle');
+      button.classList.add('fill_circle');
       button.disabled = 'true';
     }
   });
